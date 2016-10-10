7f94e34f1575a2cb8512ecaf3fe53003289f4d8b
Sebastian Lackner <sebastian@fds-team.de>
2016-10-11 01:08:03 +0200

    Add some tests to verify functionality when matlab compare files are not present.

--- tests/tests.py
+++ tests/tests.py
@@ -20,25 +20,49 @@ class Tests(unittest.TestCase):
 
         # Test rgb2gray
         img_gray = rgb2gray(img)
-        expected = numpy.loadtxt("img_gray.txt", delimiter='\t')
-        self.assertTrue(numpy.allclose(img_gray, expected, atol=1e-4))
+        self.assertEqual(tuple(img_gray.shape), (478, 640))
+        self.assertTrue(numpy.allclose(img_gray[:4, :4], numpy.array([[ 0.8973, 0.8973, 0.8973, 0.9052 ],
+                                                                      [ 0.8973, 0.8973, 0.8973, 0.9052 ],
+                                                                      [ 0.8973, 0.8973, 0.9021, 0.9061 ],
+                                                                      [ 0.9013, 0.9013, 0.9061, 0.9100 ]]), atol=1e-4))
+        if os.path.exists("img_gray.txt"):
+            expected = numpy.loadtxt("img_gray.txt", delimiter='\t')
+            self.assertTrue(numpy.allclose(img_gray, expected, atol=1e-4))
 
         # Test vl_imsmooth
         binsize = 8
         magnif  = 3
         img_smooth = vlfeat.vl_imsmooth(img_gray, math.sqrt((binsize / magnif)**2 - 0.25))
-        expected = numpy.loadtxt("img_smooth.txt", delimiter='\t')
-        self.assertTrue(numpy.allclose(img_smooth, expected, atol=1e-4))
+        self.assertEqual(tuple(img_smooth.shape), (478, 640))
+        self.assertTrue(numpy.allclose(img_smooth[:4, :4], numpy.array([[ 0.8998, 0.9013, 0.9034, 0.9057 ],
+                                                                        [ 0.9000, 0.9015, 0.9035, 0.9057 ],
+                                                                        [ 0.9002, 0.9017, 0.9036, 0.9057 ],
+                                                                        [ 0.9005, 0.9020, 0.9038, 0.9058 ]]), atol=1e-4))
+        if os.path.exists("img_smooth.txt"):
+            expected = numpy.loadtxt("img_smooth.txt", delimiter='\t')
+            self.assertTrue(numpy.allclose(img_smooth, expected, atol=1e-4))
 
         # Test vl_dsift
         frames, descrs = vlfeat.vl_dsift(img_smooth, size=binsize)
         frames = numpy.add(frames.transpose(), 1)
         descrs = descrs.transpose()
-        expected = numpy.loadtxt("dsift_frames.txt", delimiter='\t')
-        self.assertTrue(numpy.allclose(frames, expected))
-        expected = numpy.loadtxt("dsift_descrs.txt", delimiter='\t')
-        self.assertEqual(descrs.shape, expected.shape)
-        self.assertTrue(numpy.linalg.norm(expected - descrs) < 28) # rounding errors?
+        self.assertEqual(tuple(frames.shape), (2, 279664))
+        self.assertEqual(tuple(descrs.shape), (128, 279664))
+        self.assertTrue(numpy.allclose(frames[:, :4], numpy.array([[13, 13, 13, 13], [13, 14, 15, 16]])))
+        self.assertTrue(numpy.allclose(descrs[:, 0], numpy.array([134,  35,   0,   0,   0,   0,   0,   5, 109,   9,   1,   0,   0,   0,   0,  61,
+                                                                    7,   2,  32,  21,   0,   0,   1,  28,   2,  13, 111,   9,   0,   0,   0,   2,
+                                                                   33, 134, 131,   0,   0,   0,   0,   0,  30,  92, 134,   0,   0,   0,   0,  19,
+                                                                   11,  42, 134,   0,   0,   0,   1,  31,   6,  20, 124,   3,   0,   0,   0,   7,
+                                                                    5, 134, 134,   0,   0,   0,   0,   0,   1,  94, 134,   0,   0,   0,   0,   0,
+                                                                    0,  34, 134,   1,   0,   0,   0,   0,   0,   4, 134,  13,   0,   2,   2,   0,
+                                                                   27,  53,  15,   0,   0,   0,   0,   1,  11,  48,  27,   2,   0,   0,   0,   0,
+                                                                    0,   5,  28,  16,   1,   0,   0,   0,   0,   2,  13,  16,   4,   5,   4,   0])))
+        if os.path.exists("dsift_frames.txt"):
+            expected = numpy.loadtxt("dsift_frames.txt", delimiter='\t')
+            self.assertTrue(numpy.allclose(frames, expected))
+        if os.path.exists("dsift_descrs.txt"):
+            expected = numpy.loadtxt("dsift_descrs.txt", delimiter='\t')
+            self.assertTrue(numpy.linalg.norm(expected - descrs) < 28) # rounding errors?
 
         # Test vl_kmeans
         centers, assigns = vlfeat.vl_kmeans(numpy.array([[1], [2], [3], [10], [11], [12]], dtype='f'), 2, quantize=True)
