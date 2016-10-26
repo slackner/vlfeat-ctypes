62aa33501416c54a2877d612c60bb6b2f42b83e6
Sebastian Lackner <sebastian@fds-team.de>
2016-10-26 05:27:57 +0200

    Use static inline functions in dsift code.

--- vlfeat/dsift.py
+++ vlfeat/dsift.py
@@ -15,7 +15,6 @@ LIB = load_library()
 np_float = c_to_np_types[c_float]
 np_double = c_to_np_types[c_double]
 
-
 class VLDsiftKeypoint(Structure):
     _fields_ = [
         ('x', c_double),
@@ -23,7 +22,6 @@ class VLDsiftKeypoint(Structure):
         ('s', c_double),
         ('norm', c_double),
     ]
-
 
 class VLDsiftDescriptorGeometry(Structure):
     _fields_ = [
@@ -34,7 +32,6 @@ class VLDsiftDescriptorGeometry(Structure):
         ('binSizeX', c_int),
         ('binSizeY', c_int),
     ]
-
 
 class VLDsiftFilter(Structure):
     _fields_ = [
@@ -69,8 +66,6 @@ class VLDsiftFilter(Structure):
     ]
 VLDsiftFilter_p = POINTER(VLDsiftFilter)
 
-### functions in the shared object
-# most of the utility functions are actually inlined and so not in the so...
 vl_dsift_new = LIB['vl_dsift_new']
 vl_dsift_new.restype = VLDsiftFilter_p
 vl_dsift_new.argtypes = [c_int, c_int]
@@ -91,9 +86,56 @@ _vl_dsift_update_buffers = LIB['_vl_dsift_update_buffers']
 _vl_dsift_update_buffers.restype = None
 _vl_dsift_update_buffers.argtypes = [VLDsiftFilter_p]
 
+def vl_dsift_set_geometry(self, geom):
+    self.geom = geom
+    _vl_dsift_update_buffers(self)
 
-# near-direct port of the c function
-# should be vectorized, if we were going to call it more than once...
+def vl_dsift_set_steps(self, step_x, step_y):
+    self.stepX = step_x
+    self.stepY = step_y
+    _vl_dsift_update_buffers(self)
+
+def vl_dsift_set_bounds(self, min_x, min_y, max_x, max_y):
+    self.boundMinX = min_x
+    self.boundMinY = min_y
+    self.boundMaxX = max_x
+    self.boundMaxY = max_y
+    _vl_dsift_update_buffers(self)
+
+def vl_dsift_set_flat_window(self, use_flat_window):
+    self.useFlatWindow = use_flat_window
+
+def vl_dsift_set_window_size(self, window_size):
+    assert window_size >= 0.0
+    self.windowSize = window_size
+
+def vl_dsift_get_keypoint_num(self):
+    return self.numFrames
+
+def vl_dsift_get_descriptor_size(self):
+    return self.descrSize
+
+def vl_dsift_get_geometry(self):
+    return self.geom
+
+def vl_dsift_get_steps(self):
+    return (self.stepX, self.stepY)
+
+def vl_dsift_get_bounds(self):
+    return (self.boundMinX, self.boundMinY, self.boundMaxX, self.boundMaxY)
+
+def vl_dsift_get_flat_window(self):
+    return self.useFlatWindow
+
+def vl_dsift_get_window_size(self):
+    return self.windowSize
+
+def vl_dsift_get_keypoints(self):
+    return self.frames
+
+def vl_dsift_get_descriptors(self):
+    return self.descrs
+
 def vl_dsift_transpose_descriptor(dest, src, num_bin_t, num_bin_x, num_bin_y):
     for y in xrange(num_bin_y):
         for x in xrange(num_bin_x):
@@ -103,6 +145,7 @@ def vl_dsift_transpose_descriptor(dest, src, num_bin_t, num_bin_x, num_bin_y):
                 tT = num_bin_t // 4 - t
                 dest[offsetT + (tT + num_bin_t) % num_bin_t] = src[offset + t]
 
+################################################################################
 
 def vl_dsift(data, fast=False, norm=False, bounds=None, size=3, step=1,
               window_size=None, float_descriptors=False,
@@ -125,51 +168,45 @@ def vl_dsift(data, fast=False, norm=False, bounds=None, size=3, step=1,
     # construct the dsift object
     M, N = data.shape
     dsift_p = vl_dsift_new_basic(M, N, step, size)
-
     try:
         dsift = dsift_p.contents
+        # vl_dsift_set_geometry(dsift, geom)
+        # vl_dsift_set_steps(dsift, step[0], step[1])
 
-        # set parameters
         if bounds is not None:
             y0, x0, y1, x1 = bounds  # transposed
-            dsift.boundMinX = int(max(x0, 0))
-            dsift.boundMinY = int(max(y0, 0))
-            dsift.boundMaxX = int(min(x1, M - 1))
-            dsift.boundMaxY = int(min(y1, N - 1))
-            _vl_dsift_update_buffers(dsift_p)
+            vl_dsift_set_bounds(dsift, int(max(x0, 0)),
+                                       int(max(y0, 0)),
+                                       int(min(x1, M - 1)),
+                                       int(min(y1, N - 1)))
 
-        dsift.useFlatWindow = fast
+        vl_dsift_set_flat_window(dsift, fast)
 
         if window_size is not None:
-            dsift.windowSize = window_size
+            vl_dsift_set_window_size(dsift, window_size)
 
-        # get calculated parameters
-        descr_size = dsift.descrSize
-        num_frames = dsift.numFrames
-        geom = dsift.geom
+        num_frames = vl_dsift_get_keypoint_num(dsift)
+        descr_size = vl_dsift_get_descriptor_size(dsift)
+        geom = vl_dsift_get_geometry(dsift)
 
         if verbose:
-            pr = lambda *a, **k: print('vl_dsift:', *a, **k)
-            pr("image size         [W, H] = [{}, {}]".format(N, M))
-            x0 = dsift.boundMinX + 1
-            y0 = dsift.boundMinY + 1
-            x1 = dsift.boundMaxX + 1
-            y1 = dsift.boundMaxY + 1
-            bound_args = [y0, x0, y1, x1]
-            pr("bounds:            [minX,minY,maxX,maxY] = [{}, {}, {}, {}]"
-                .format(*bound_args))
-            pr("subsampling steps: stepX={}, stepY={}".format(
-                    dsift.stepX, dsift.stepY))
-            pr("num bins:          [numBinT, numBinX, numBinY] = [{}, {}, {}]"
-                .format(geom.numBinT, geom.numBinX, geom.numBinY))
-            pr("descriptor size:   {}".format(descr_size))
-            pr("bin sizes:         [binSizeX, binSizeY] = [{}, {}]".format(
-                    geom.binSizeX, geom.binSizeY))
-            pr("flat window:       {}".format(bool(fast)))
-            pr("window size:       {}".format(dsift.windowSize))
-            pr("num of features:   {}".format(num_frames))
+            step_x, step_y = vl_dsift_get_steps(dsift)
+            min_y, min_x, max_y, max_x = vl_dsift_get_bounds(dsift)
+            use_flat_window = vl_dsift_get_flat_window(dsift)
 
-        # do the actual processing
+            print("vl_dsift: image size         [W, H] = [{}, {}]".format(N, M))
+            print("vl_dsift: bounds:            [minX,minY,maxX,maxY] = [{}, {}, {}, {}]"
+                .format(min_x + 1, min_y + 1, max_x + 1, max_y + 1))
+            print("vl_dsift: subsampling steps: stepX={}, stepY={}".format(step_x, step_y))
+            print("vl_dsift: num bins:          [numBinT, numBinX, numBinY] = [{}, {}, {}]"
+                .format(geom.numBinT, geom.numBinX, geom.numBinY))
+            print("vl_dsift: descriptor size:   {}".format(descr_size))
+            print("vl_dsift: bin sizes:         [binSizeX, binSizeY] = [{}, {}]"
+                .format(geom.binSizeX, geom.binSizeY))
+            print("vl_dsift: flat window:       {}".format(bool(use_flat_window)))
+            print("vl_dsift: window size:       {}".format(vl_dsift_get_window_size(dsift)))
+            print("vl_dsift: num of features:   {}".format(num_frames))
+
         vl_dsift_process(dsift_p, data)
 
         # copy frames' locations, norms out
@@ -177,7 +214,7 @@ def vl_dsift(data, fast=False, norm=False, bounds=None, size=3, step=1,
         # which luckily looks exactly like an array of doubles. :)
         # NOTE: this might be platform/compiler-dependent...but it works with
         #       the provided binaries on os x, at least
-        frames_p = cast(dsift.frames, c_double_p)
+        frames_p = cast(vl_dsift_get_keypoints(dsift), c_double_p)
         frames_p_a = npc.as_array(frames_p, shape=(num_frames, 4))
         cols = [1, 0]
         if norm:
@@ -185,7 +222,7 @@ def vl_dsift(data, fast=False, norm=False, bounds=None, size=3, step=1,
         frames = np.require(frames_p_a[:, cols], requirements=['C', 'O'])
 
         # copy descriptors into a new array
-        descrs_p = npc.as_array(dsift.descrs, shape=(num_frames, descr_size))
+        descrs_p = npc.as_array(vl_dsift_get_descriptors(dsift), shape=(num_frames, descr_size))
         descrs = descrs_p * 512
         assert descrs.flags.owndata
         np.minimum(descrs, 255, out=descrs)
